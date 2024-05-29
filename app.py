eb6462721b2f6c2246b670d6219b8ec8a12eec8f
AnselmoNoriega <anselmonoriega3@gmail.com>
2024-05-29 10:04:21 -0700

    app with chatgpt

--- app.py
+++ app.py
@@ -10,51 +10,32 @@ def receive_data():
     if not data:
         return jsonify({'error': 'No JSON payload provided'}), 400
 
-    first_string = data.get('firstString')
-    second_string = data.get('secondString')
+    api_key = data.get('ApiKey')
+    ASSISTANT_ID = data.get('AssistentID')
+    prompt = data.get('Prompt')
 
-    if first_string is None or second_string is None:
-        return jsonify({'error': 'Both "firstString" and "secondString" keys must be present in JSON payload'}), 400
+    client = OpenAI(api_key)
+    
+    thread = client.beta.threads.create()
 
-    combined = f"{first_string} {second_string}"
-    return jsonify({'combined': combined})
-
-"""
-client = OpenAI(api_key = "")
-
-assistant = client.beta.assistants.create(
-    name = "Roby",
-    instructions = "Your are a math tutor. Write and run code to answer questions",
-    tools = [{"type": "code_interpreter"}],
-    model = "gpt-3.5-turbo"
+    message = client.beta.threads.messages.create(
+        thread_id = thread.id,
+        role = "user",
+        content = prompt
     )
 
-ASSISTANT_ID = ""
+    run = client.beta.threads.runs.create(
+        thread_id = thread.id,
+        assistant_id = ASSISTANT_ID
+    )
 
-thread = client.beta.threads.create()
+    while run.status != "completed":
+        run = client.beta.threads.runs.retrieve(thread_id = thread.id, run_id = run.id)
+        time.sleep(1)
 
-message = client.beta.threads.messages.create(
-    thread_id = thread.id,
-    role = "user",
-    content = "solve 3 plus 5"
-)
-
-run = client.beta.threads.runs.create(
-    thread_id = thread.id,
-    assistant_id = assistant.id
-)
-
-while run.status != "completed":
-    run = client.beta.threads.runs.retrieve(thread_id = thread.id, run_id = run.id)
-    print("Run Status: {run.status}")
-    time.sleep(1)
-else:
-    print("Finished")
-    
-messages = client.beta.threads.messages.list(
+    messages = client.beta.threads.messages.list(
     thread_id = thread.id
-)
+    )
 
-for it in reversed(messages.data):
-    print(it.role + ": " + it.content[0].text.value)
-"""
+    for it in reversed(messages.data):
+        return jsonify(it.content[0].text.value)
