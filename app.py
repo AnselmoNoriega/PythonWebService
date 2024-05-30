83b2267888b5738a39ed1d1a74ab2fd9afe6b99e
AnselmoNoriega <anselmonoriega3@gmail.com>
2024-05-30 14:45:42 -0700

    second to last loop

--- app.py
+++ app.py
@@ -37,6 +37,6 @@ def receive_data():
     messages = client.beta.threads.messages.list(thread_id = thread.id)
 
     for it in messages.data:
-            return jsonify(it.content[0].text.value)
+            return jsonify(messages.data[-2].content[0].text.value)
     
     return jsonify(run.status)
