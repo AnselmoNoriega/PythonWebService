cd9d65813677a7e290ee3bc5be9b8899d65662b1
AnselmoNoriega <anselmonoriega3@gmail.com>
2024-05-30 17:50:37 -0700

    Testing setup

--- app.py
+++ app.py
@@ -4,41 +4,29 @@ from openai import OpenAI
 
 app = Flask(__name__)
 
-@app.route('/', methods = ['POST'])
-def receive_data():
-    data = request.get_json()
-    if not data:
-        return jsonify({'error': 'No JSON payload provided'}), 400
 
-    keyID = "sk-proj-UJlJP7IIAccXodn8iARnT3BlbkFJV8PWozcARMGmt2Gf7CA7"
-    assistantID = "asst_XOyqJAlc7oJuEKCa1blkFyT7"
-    promptHistory = data.get('PromptHistory')
-    
-    client = OpenAI(api_key = keyID)
-    
-    thread = client.beta.threads.create()
+keyID = "sk-proj-UJlJP7IIAccXodn8iARnT3BlbkFJV8PWozcARMGmt2Gf7CA7"
+assistantID = "asst_XOyqJAlc7oJuEKCa1blkFyT7"
+promptHistory = ["Whats your name", "What is the biggest animal"]
 
-    for prevPromt in promptHistory:
-        client.beta.threads.messages.create(
-            thread_id = thread.id,
-            role = "user",
-            content = prevPromt
-        )
+client = OpenAI(api_key = keyID)
 
-    run = client.beta.threads.runs.create(
+thread = client.beta.threads.create()
+for prevPromt in promptHistory:
+    client.beta.threads.messages.create(
         thread_id = thread.id,
-        assistant_id = assistantID
+        role = "user",
+        content = prevPromt
     )
-
-    while run.status != "completed":
-        run = client.beta.threads.runs.retrieve(thread_id = thread.id, run_id = run.id)
-        time.sleep(1)
-
-    messages = client.beta.threads.messages.list(thread_id = thread.id)
-
-    stri = []
-    for it in messages.data:
-        stri.append(it.content[0].text.value)
-    return jsonify(stri)
-    
-    return jsonify(run.status)
+run = client.beta.threads.runs.create(
+    thread_id = thread.id,
+    assistant_id = assistantID
+)
+while run.status != "completed":
+    run = client.beta.threads.runs.retrieve(thread_id = thread.id, run_id = run.id)
+    time.sleep(1)
+messages = client.beta.threads.messages.list(thread_id = thread.id)
+stri = []
+for it in messages.data:
+    stri.append(it.content[0].text.value + "\n")
+print(stri)
