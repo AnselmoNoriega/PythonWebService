5e8c93b8750a2d27c921f70fc0f2c97212e3e742
AnselmoNoriega <anselmonoriega3@gmail.com>
2024-06-01 15:27:36 -0700

    new test

--- app.py
+++ app.py
@@ -7,17 +7,19 @@ app = Flask(__name__)
 
 keyID = "sk-proj-UJlJP7IIAccXodn8iARnT3BlbkFJV8PWozcARMGmt2Gf7CA7"
 assistantID = "asst_XOyqJAlc7oJuEKCa1blkFyT7"
-promptHistory = ["Whats your name", "What is the biggest animal"]
+prompt = "Whats your name"
 
 client = OpenAI(api_key = keyID)
 
 thread = client.beta.threads.create()
-for prevPromt in promptHistory:
-    client.beta.threads.messages.create(
-        thread_id = thread.id,
-        role = "user",
-        content = prevPromt
-    )
+thread.id = "thread_dX5ItcTiOWRY0cEsAs5yAMm5"
+
+client.beta.threads.messages.create(
+    thread_id = thread.id,
+    role = "user",
+    content = prompt
+)
+
 run = client.beta.threads.runs.create(
     thread_id = thread.id,
     assistant_id = assistantID
@@ -25,8 +27,28 @@ run = client.beta.threads.runs.create(
 while run.status != "completed":
     run = client.beta.threads.runs.retrieve(thread_id = thread.id, run_id = run.id)
     time.sleep(1)
+    
 messages = client.beta.threads.messages.list(thread_id = thread.id)
-stri = []
-for it in messages.data:
-    stri.append(it.content[0].text.value + "\n")
-print(stri)
+print(messages.data[0].content[0].text.value)
+
+client = OpenAI(api_key = keyID)
+thread = client.beta.threads.create()
+thread.id = "thread_dX5ItcTiOWRY0cEsAs5yAMm5"
+prompt = "What was my first question and whats 3 plus 3"
+
+client.beta.threads.messages.create(
+    thread_id = thread.id,
+    role = "user",
+    content = prompt
+)
+
+run = client.beta.threads.runs.create(
+    thread_id = thread.id,
+    assistant_id = assistantID
+)
+while run.status != "completed":
+    run = client.beta.threads.runs.retrieve(thread_id = thread.id, run_id = run.id)
+    time.sleep(1)
+    
+messages = client.beta.threads.messages.list(thread_id = thread.id)
+print(messages.data[0].content[0].text.value)
