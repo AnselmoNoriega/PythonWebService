7249f93f2e5ac01bea1b23bf9a6bfe22f9db5848
AnselmoNoriega <anselmonoriega3@gmail.com>
2024-06-06 13:02:55 -0700

    CORS addition

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import time
 from openai import OpenAI
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/senddata', methods = ['POST'])
 def receive_data():
