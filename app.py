94b45f3ddd0a731bdd176b59fb8397947e770b61
AnselmoNoriega <anselmonoriega3@gmail.com>
2024-06-23 20:17:57 -0700

    update error wav

--- app.py
+++ app.py
@@ -50,9 +50,11 @@ def convert_bytes_to_floats():
         
     mp3_audio = AudioSegment.from_file(io.BytesIO(mp3_data), format='mp3')
     
-    wav_data = mp3_audio.set_frame_rate(44100).set_channels(2).raw_data(convert_to="WAV")
+    wav_data = io.BytesIO()
+    mp3_audio.export(wav_data, format="wav")
     
-    return send_file(io.BytesIO(wav_data), mimetype="audio/wav", as_attachment=True, attachment_filename="converted.wav")
+    wav_data.seek(0)
+    return send_file(wav_data, mimetype="audio/wav", as_attachment=True, attachment_filename="converted.wav")
     
 
 @app.route('/getdata', methods = ['GET'])
